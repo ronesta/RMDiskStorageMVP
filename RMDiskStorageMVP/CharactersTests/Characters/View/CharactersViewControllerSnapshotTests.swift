10bf20d6fca0802ebc0cf23ee654581bd94793dd
ronesta <gvozzzd123@gmail.com>
2025-04-15 19:32:14 +0300

    CheckPoint

--- RMDiskStorageMVP/CharactersTests/Characters/View/CharactersViewControllerSnapshotTests.swift
+++ RMDiskStorageMVP/CharactersTests/Characters/View/CharactersViewControllerSnapshotTests.swift
@@ -10,7 +10,7 @@ import SnapshotTesting
 @testable import RMDiskStorageMVP
 
 final class CharactersViewControllerSnapshotTests: XCTestCase {
-    func testCharactersViewControllerDefaultAppearance() {
+    func testCharactersViewControllerAppearance() {
         let presenter = MockPresenter()
         let dataSource = MockDataSource()
 
--- RMDiskStorageMVP/CharactersTests/Network/ImageLoader/MockImageLoader.swift
+++ RMDiskStorageMVP/CharactersTests/Network/ImageLoader/MockImageLoader.swift
@@ -5,7 +5,7 @@
 //  Created by Ибрагим Габибли on 05.04.2025.
 //
 
-import UIKit
+import UIKit.UIImage
 @testable import RMDiskStorageMVP
 
 final class MockImageLoader: ImageLoaderProtocol {
